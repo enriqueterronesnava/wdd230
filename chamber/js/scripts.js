1f41c403f7fd152db35d06f63b1dc2ecb51be45c
EnriqueTN <ter21010@byui.edu>
2022-06-04 15:13:18 -0500

    w7

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -8,5 +8,20 @@ const x = document.getElementById("hamburgerbtn");
 x.onclick = togglemenu;
 
 let c = (" Last modified: " + document.lastModified);
-document.getElementById("lastmod").textContent = c
-document.getElementById("lastmod2").textContent = c
+document.getElementById("lastmod").textContent = c;
+document.getElementById("lastmod2").textContent = c;
+
+const timedisplay = document.querySelector(".timedisplay");
+
+localStorage.setItem("newdate", Date.now());
+let olddate = Number(localStorage.getItem("olddate"));
+let newdate = Number(localStorage.newdate);
+if (olddate == 0) {
+    olddate = newdate;
+}
+let timepassed = newdate - olddate;
+if (timepassed !==0) {
+    timedisplay.textContent = "Last visit since: " + Math.round(timepassed/8.64e+7) +" "+"days";}
+    else{timedisplay.textContent = `This is your first visit!`;};
+
+localStorage.setItem("olddate", newdate);
