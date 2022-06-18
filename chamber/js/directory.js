652a5f184f4d4a44ae15efef8ed58720581c64f3
EnriqueTN <ter21010@byui.edu>
2022-06-18 12:54:11 -0500

    w9

---
+++ chamber/js/directory.js
@@ -0,0 +1,43 @@
+function togglemenu() {
+    document.getElementById("primarynav").classList.toggle("open");
+    document.getElementById("hamburgerbtn").classList.toggle("open");
+    
+}
+
+const x = document.getElementById("hamburgerbtn");
+x.onclick = togglemenu;
+
+let c = (" Last modified: " + document.lastModified);
+document.getElementById("lastmod").textContent = c;
+document.getElementById("lastmod2").textContent = c;
+
+
+const requestURL = 'C:\Users\enriq\OneDrive\Documentos\Programación\WebFrontend\wdd230\chamber\js\data.json';
+const cards = document.querySelector('.cards');
+
+async function getcompanies() {
+    let response = await fetch(requestURL);
+    if (response.ok) {
+    let data = await response.json();
+    //console.log(data);
+    buildPcards(data);
+    } else {
+        throw Error(response.statusText);
+    }
+}
+
+function getcompanies(data){
+    data.companies.forEach(companie => {
+        let card = document.createElement("section");
+        let name = document.createElement("h2");
+        let address = document.createElement("p");
+        
+
+        name.innerHTML = `Name: ${companie.name}`;
+        address.innerHTML = `Address: ${companie.address}`
+        card.append(name);
+        card.append
+    });
+}
+
+getcompanies()
